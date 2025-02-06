994ea061c5c3bcdd0ac841551a744e5171cef6ca
SajidGani <sajidsgani@gmail.com>
2025-02-06 18:05:34 -0500

    git commit

--- src/main/java/com/example/demo/WeatherController.java
+++ src/main/java/com/example/demo/WeatherController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,9 +9,9 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherController {
 
     @GetMapping("/weather")
-    public String getWeather() {
-        // Replace 'London' with any city you want
-        String url = "http://api.weatherstack.com/current?access_key=0894ae5155d2e9c7046011733b6fa2c9&query=Nagercoil";
+    public String getWeather(@RequestParam String query) {
+        // Use the query parameter for the location
+        String url = "http://api.weatherstack.com/current?access_key=0894ae5155d2e9c7046011733b6fa2c9&query=" + query;
 
         // Initialize RestTemplate
         RestTemplate restTemplate = new RestTemplate();
